5a535ba3b01b1efc301aa63b454fbbf9c4a3a456
dinu1006 <dinu.minnu357@gmail.com>
2025-12-26 00:44:42 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,28 @@ import "./components/header.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en">  
+      <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      const observer = new IntersectionObserver(
+        entries => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add("show");
+            }
+          });
+        },
+        { threshold: 0.15 }
+      );
+
+      document.querySelectorAll("[data-animate]").forEach(el => {
+        observer.observe(el);
+      });
+    `,
+  }}
+/>
+
       <body>
         <Header />
         {children}
