4dfcad506b2e9f000f9bdda3643e1b20131dbb02
dinu1006 <dinu.minnu357@gmail.com>
2025-12-25 15:19:32 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer style={styles.footer}>
+      © 2025 Sri Raksha Hospital. All Rights Reserved.
+    </footer>
+  );
+}
+
+const styles = {
+  footer: {
+    background: "#1c1c1c",
+    color: "#aaa",
+    textAlign: "center",
+    padding: "20px",
+    marginTop: "60px",
+    fontSize: "14px"
+  }
+};
