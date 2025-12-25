e2c511adfe2a417560f48f33da8f9652c08762ff
dinu1006 <dinu.minnu357@gmail.com>
2025-12-25 16:37:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import type { ReactNode } from "react";
 
 export const metadata = {
   title: "Sri Raksha Hospital",
@@ -9,7 +8,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
